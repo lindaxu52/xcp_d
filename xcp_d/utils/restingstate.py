0548b63c6aa2f21d271cde259e86ea11a446245b
lindaxu52 <linda.b.xu@gmail.com>
2023-12-03 16:26:18 -0500

    Update restingstate.py

--- xcp_d/utils/restingstate.py
+++ xcp_d/utils/restingstate.py
@@ -132,9 +132,9 @@ def compute_alff(data_matrix, low_pass, high_pass, TR):
         # alff for that voxel is 2 * the mean of the sqrt of the power spec density
         # from the value closest to the low pass cutoff, to the value closest
         # to the high pass pass cutoff
-        # alff[ii] = len(ff_alff) * np.mean(power_spec_density_sqrt[ff_alff[0] : ff_alff[1]])
+        alff[ii] = len(ff_alff) * np.mean(power_spec_density_sqrt[ff_alff[0] : ff_alff[1]])
         # run alff but it's actually falff
-        alff[ii] = np.sum(power_spec_density_sqrt[ff_alff[0]: ff_alff[1]]) / np.sum(power_spec_density_sqrt[ff_alff[0]:])
+        # alff[ii] = np.sum(power_spec_density_sqrt[ff_alff[0]: ff_alff[1]]) / np.sum(power_spec_density_sqrt[ff_alff[0]:])
 
     # reshape alff so it's no longer 1 dimensional, but a #ofvoxels by 1 matrix
     alff = np.reshape(alff, [len(alff), 1])
