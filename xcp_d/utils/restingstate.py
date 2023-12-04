481592b778d60829dd6486e64fc1ec883feac5e3
lindaxu52 <linda.b.xu@gmail.com>
2023-12-04 10:48:26 -0500

    Update restingstate.py

--- xcp_d/utils/restingstate.py
+++ xcp_d/utils/restingstate.py
@@ -130,8 +130,8 @@ def compute_alff(data_matrix, low_pass, high_pass, TR):
             np.argmin(np.abs(array_of_sample_frequencies - low_pass)),
         ]
         # run alff but it's actually falff
-        num = np.mean(power_spec_density_sqrt[ff_alff[0]:ff_alff[1]])
-        denom = np.mean(power_spec_density_sqrt[ff_alff[0]:])
+        num = np.mean(power_spec_density_sqrt[ff_alff[0] : ff_alff[1]])
+        denom = np.mean(power_spec_density_sqrt[ff_alff[0] : ])
         alff[ii] = num / denom
         # alff for that voxel is 2 * the mean of the sqrt of the power spec density
         # from the value closest to the low pass cutoff, to the value closest
