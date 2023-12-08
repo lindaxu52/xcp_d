278e912d9a64d4f3aefe2898b8be673f9a9df9b1
lindaxu52 <linda.b.xu@gmail.com>
2023-12-08 14:06:08 -0500

    Update restingstate.py

--- xcp_d/utils/restingstate.py
+++ xcp_d/utils/restingstate.py
@@ -119,15 +119,17 @@ def compute_alff(data_matrix, low_pass, high_pass, TR):
     # number of voxels
     for ii in range(data_matrix.shape[0]):  # Loop through the voxels
         # get array of sample frequencies + power spectrum density
+        # periodogram outputs all frequencies with associated power 
         array_of_sample_frequencies, power_spec_density = signal.periodogram(
             data_matrix[ii, :], fs, scaling="spectrum"
         )
         # square root of power spectrum density
         power_spec_density_sqrt = np.sqrt(power_spec_density)
         # get the position of the arguments closest to high_pass and low_pass, respectively
+        # creates a 
         ff_alff = [
-            np.argmin(np.abs(array_of_sample_frequencies - 0.01)),
-            np.argmin(np.abs(array_of_sample_frequencies - 0.08)),
+            np.argmin(np.abs(array_of_sample_frequencies - 0.1)),
+            np.argmin(np.abs(array_of_sample_frequencies - 0.25)),
         ]
         ff_alff2 = [
             np.argmin(np.abs(array_of_sample_frequencies - 0.01)),
